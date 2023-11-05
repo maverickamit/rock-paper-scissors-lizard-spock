0b7e74522f2f310543d97cb959117516cb430aee
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-11-05 19:36:14 +0530

    Use try catch for generateSalt

--- src/utils/generateSalt.ts
+++ src/utils/generateSalt.ts
@@ -1,22 +1,25 @@
 // Generate a random uint256 salt using BigInt
 async function generateSalt() {
-  if (typeof window.crypto === "undefined") {
-    throw new Error("window.crypto is not available in this environment");
+  try {
+    if (typeof window.crypto === "undefined") {
+      throw new Error("window.crypto is not available in this environment");
+    }
+    // Generate a random 32-byte (256-bit) array
+    const randomBytes = new Uint8Array(32);
+    window.crypto.getRandomValues(randomBytes);
+
+    // Convert the random bytes to a BigInt
+    const salt = BigInt(
+      "0x" +
+        Array.from(randomBytes)
+          .map((byte) => byte.toString(16).padStart(2, "0"))
+          .join("")
+    );
+
+    return salt;
+  } catch (error) {
+    console.error("Error generating salt:", error);
   }
-
-  // Generate a random 32-byte (256-bit) array
-  const randomBytes = new Uint8Array(32);
-  window.crypto.getRandomValues(randomBytes);
-
-  // Convert the random bytes to a BigInt
-  const salt = BigInt(
-    "0x" +
-      Array.from(randomBytes)
-        .map((byte) => byte.toString(16).padStart(2, "0"))
-        .join("")
-  );
-
-  return salt;
 }
 
 export default generateSalt;
